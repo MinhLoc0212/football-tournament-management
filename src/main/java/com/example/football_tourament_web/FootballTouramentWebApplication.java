95f6395912125ed3c3f8c058f22cb209c6e2b18f
qminh <133646909+KENTksl@users.noreply.github.com>
2026-03-16 19:08:41 +0700

    feat: initialize Spring Boot project structure

---
+++ src/main/java/com/example/football_tourament_web/FootballTouramentWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.football_tourament_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballTouramentWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballTouramentWebApplication.class, args);
+	}
+
+}
