eac8aba5c639e7ccc70eea30a29730becd331864
qminh <133646909+KENTksl@users.noreply.github.com>
2026-03-20 20:31:50 +0700

    fix: update flow page

--- src/main/java/com/example/football_tourament_web/controller/admin/AdminController.java
+++ src/main/java/com/example/football_tourament_web/controller/admin/AdminController.java
@@ -1,4 +1,4 @@
-package com.example.football_tourament_web;
+package com.example.football_tourament_web.controller.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AdminController {
 
-	@GetMapping({"/", "/admin/general-overview"})
+	@GetMapping({"/admin", "/admin/general-overview"})
 	public String generalOverview() {
-		return "/admin/general-overview";
+		return "admin/general-overview";
 	}
 }
 
