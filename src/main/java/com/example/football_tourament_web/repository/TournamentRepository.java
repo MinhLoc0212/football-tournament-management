b654e7c5782840ba396e9af944a066e4234b4381
tuankiet0811 <kiettn2k4@gmail.com>
2026-03-24 15:45:04 +0700

    fix: sửa lỗi logic lấy dữ liệu lần 2

--- src/main/java/com/example/football_tourament_web/repository/TournamentRepository.java
+++ src/main/java/com/example/football_tourament_web/repository/TournamentRepository.java
@@ -3,6 +3,7 @@ package com.example.football_tourament_web.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import org.springframework.data.jpa.repository.Query;
 import com.example.football_tourament_web.model.entity.Tournament;
